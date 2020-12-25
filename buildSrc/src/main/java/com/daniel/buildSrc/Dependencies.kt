989bfacf0a02aef728929d1384f862547d13a6d4
Daniel Gomes <daniel0x7cc@gmail.com>
2020-12-25 02:14:37 -0300

    [Feature] Added module buildSrc for organizing dependencies and its versions

---
+++ buildSrc/src/main/java/com/daniel/buildSrc/Dependencies.kt
@@ -0,0 +1,39 @@
+package com.daniel.buildSrc
+
+/**
+ * File that contains all the dependencies used for this project.
+ */
+
+object Dependencies {
+
+    object Core {
+        const val appcompat: String = "androidx.appcompat:appcompat:" + Versions.app_compat
+
+        const val core_ktx: String = "androidx.core:core-ktx:" + Versions.core_ktx
+
+        const val kotlin_stdlib_jdk8: String =
+            "org.jetbrains.kotlin:kotlin-stdlib-jdk8:" + Versions.kotlin
+    }
+
+
+    object Tool {
+        const val gradle: String = "com.android.tools.build:gradle:" + Versions.gradle
+
+        const val gradle_kotlin_plugin: String = "org.jetbrains.kotlin:kotlin-gradle-plugin:" + Versions.kotlin
+
+        const val ktlint: String = "com.pinterest:ktlint:" + Versions.ktlint
+    }
+
+    object UI {
+        const val constraint_layout: String = "androidx.constraintlayout:constraintlayout:" +
+                Versions.constraint_layout
+
+        const val material: String = "com.google.android.material:material:" + Versions.material
+    }
+
+    object Test {
+        const val junit: String = "junit:junit:" + Versions.junit
+
+        const val espresso: String = "androidx.test.espresso:espresso-core:" + Versions.espresso
+    }
+}
---
+++ buildSrc/src/main/java/com/daniel/buildSrc/Versions.kt
@@ -0,0 +1,30 @@
+package com.daniel.buildSrc
+
+/**
+ * File that contains all the dependencies versions used for this project.
+ */
+
+object Versions {
+    /**
+     * Build.Gradle configuration versions
+     */
+    const val compile_sdk = 30
+    const val build_tools = "30.0.0"
+    const val jvm_target_version = "1.8"
+    const val min_sdk = 21
+    const val target_sdk = 30
+    const val version_code = 1
+    const val version_name = "1.0"
+    /**
+     * External dependency versions
+     */
+    const val app_compat = "1.2.0"
+    const val constraint_layout = "2.0.4"
+    const val core_ktx = "1.2.0"
+    const val gradle = "4.1.0"
+    const val kotlin = "1.3.72"
+    const val ktlint = "0.36.0"
+    const val material = "1.2.1"
+    const val junit = "4.12"
+    const val espresso = "3.3.0"
+}
